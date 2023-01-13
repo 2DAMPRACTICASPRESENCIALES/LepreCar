a5f3812f7b457fdabd58908485259b2ddab3eb43
Juan Senen <juansenen@hotmail.com>
2023-01-13 15:00:29 +0100

    Service and Repository

---
+++ src/main/java/com/svalero/leprecar/service/CarService.java
@@ -0,0 +1,9 @@
+package com.svalero.leprecar.service;
+
+import com.svalero.leprecar.domain.Car;
+
+import java.util.List;
+
+public interface CarService {
+    List<Car> findAll();
+}
---
+++ src/main/java/com/svalero/leprecar/service/CarServiceImpl.java
@@ -0,0 +1,14 @@
+package com.svalero.leprecar.service;
+
+import com.svalero.leprecar.domain.Car;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    @Override
+    public List<Car> findAll() {
+        return null;
+    }
+}
