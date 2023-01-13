ce0c351cd25cffbdcfbda733c526477ee30e6ea8
FernandoCamposSegura <campossegura19@gmail.com>
2023-01-13 15:00:42 +0100

    Añadido repositorio y service User

---
+++ src/main/java/com/svalero/leprecar/service/UserService.java
@@ -0,0 +1,9 @@
+package com.svalero.leprecar.service;
+
+import com.svalero.leprecar.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> findAll();
+}
---
+++ src/main/java/com/svalero/leprecar/service/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.svalero.leprecar.service;
+
+import com.svalero.leprecar.domain.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+}
