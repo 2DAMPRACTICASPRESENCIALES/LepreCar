4fcc85642e68106a304d78176d1f26c6c6bab21c
Juan Senen <juansenen@hotmail.com>
2023-01-13 12:37:14 +0100

    Inicio

---
+++ src/main/java/com/svalero/leprecar/LeprecarApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.leprecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeprecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeprecarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/leprecar/LeprecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.leprecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeprecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
