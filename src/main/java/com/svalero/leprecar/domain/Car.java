c3d08acb2d44e192df404470a8b12e4fc96bf34a
Juan Senen <juansenen@hotmail.com>
2023-01-13 13:30:06 +0100

    Añadido relacion coches reservas

--- src/main/java/com/svalero/leprecar/domain/Car.java
+++ src/main/java/com/svalero/leprecar/domain/Car.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -27,4 +29,8 @@ public class Car {
     private int km;
     @Column
     private float hourPrice;
+
+    //Relación cars con booking
+    @OneToMany(mappedBy = "car",cascade = CascadeType.REMOVE, orphanRemoval = true) //Para borrar en cascada
+    private List<Booking> bookings;
 }
